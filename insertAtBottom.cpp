139683979c51766a3587b044cf6ef102908525f4
Mohit Sati <146699961+SuperexMack@users.noreply.github.com>
2023-10-21 19:08:51 +0530

    Update insertAtBottom.cpp

--- insertAtBottom.cpp
+++ insertAtBottom.cpp
@@ -1,54 +1,47 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 
-void mycode(stack<int>&str , int x){
+void mycode(stack<int> &str, int x)
+{
 
-// Base Case
+    // Base Case
 
-if(str.empty()){
+    if (str.empty())
+    {
 
-str.push(x); // using this we are inserting the element in a array when the array is empty
+        str.push(x); // using this we are inserting the element in a array when the array is empty
 
-return;
+        return;
+    }
 
+    int ans = str.top();
+    str.pop();
+
+    mycode(str, x); // using this we are using recursion
+
+    str.push(ans);
 }
 
-int ans = str.top();
-str.pop();
+int main()
+{
 
+    stack<int> str;
 
+    str.push(21);
+    str.push(45);
+    str.push(989);
+    str.push(23);
+    str.push(432);
 
-mycode(str,x); // using this we are using recursion
+    cout << str.top() << endl;
 
-str.push(ans);
+    mycode(str, 69);
 
+    while (!str.empty())
+    {
 
-}
-
-
-int main(){
-
-stack<int>str;
-
-str.push(21);
-str.push(45);
-str.push(989);
-str.push(23);
-str.push(432);
-
-cout<<str.top()<<endl;
-
-mycode(str , 69);
-
-while(!str.empty()){
-
-int mom = str.top();
-cout<<mom<<endl;
-str.pop();
-
-
-
-}
-
-
+        int mom = str.top();
+        cout << mom << endl;
+        str.pop();
+    }
 }
